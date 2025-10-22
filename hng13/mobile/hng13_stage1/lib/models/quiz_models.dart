b10967a99a276053e9e8fe0e5cba1691d0e40325
lurldgbodex <segungbodise@gmail.com>
2025-10-22 14:01:24 +0100

    feat: quiz model class

---
+++ hng13/mobile/hng13_stage1/lib/models/quiz_models.dart
@@ -0,0 +1,13 @@
+class Question {
+  final String question;
+  final List<String> options;
+  final int correctAnswerIndex;
+  final String category;
+
+  Question({
+    required this.question,
+    required this.options,
+    required this.correctAnswerIndex,
+    required this.category,
+  });
+}
