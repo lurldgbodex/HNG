f5dc6b46289859e6c1e372886364c38a3eee18a9
lurldgbodex <segungbodise@gmail.com>
2025-10-30 08:19:01 +0100

    fix: broken links in deployed app

--- hng13/frontend/hng13-stage2/ticketapp-vue/vite.config.ts
+++ hng13/frontend/hng13-stage2/ticketapp-vue/vite.config.ts
@@ -1,8 +1,34 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  build: {
+    outDir: "dist",
+  },
+  define: {},
+  esbuild: {},
+  css: {},
+  optimizeDeps: {},
+  server: {},
+  plugins: [
+    vue(),
+    tailwindcss(),
+    {
+      name: "copy-redirects",
+      closeBundle() {
+        const src = resolve(__dirname, "public/_redirects");
+        const dest = resolve(__dirname, "dist/_redirects");
+        if (fs.existsSync(src)) {
+          fs.copyFileSync(src, dest);
+          console.log("Copied _redirects to dist folder");
+        } else {
+          console.warn("_redirects file does not exist in public folder");
+        }
+      },
+    },
+  ],
 });
