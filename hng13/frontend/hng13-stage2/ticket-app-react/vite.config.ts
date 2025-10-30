8e728bc9a255e501a7bf0a020ec02c2a9bed3621
lurldgbodex <segungbodise@gmail.com>
2025-10-30 08:11:21 +0100

    fix: netlify deployment broken link

--- hng13/frontend/hng13-stage2/ticket-app-react/vite.config.ts
+++ hng13/frontend/hng13-stage2/ticket-app-react/vite.config.ts
@@ -1,8 +1,34 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist",
+  },
+  define: {},
+  esbuild: {},
+  css: {},
+  optimizeDeps: {},
+  server: {},
+  plugins: [
+    react(),
+    tailwindcss(),
+    {
+      name: "copy-redirects",
+      closeBundle() {
+        const src = resolve(__dirname, "public/_redirects");
+        const dest = resolve(__dirname, "dist/_redirects");
+        if (fs.existsSync(src)) {
+          fs.copyFileSync(src, dest);
+          console.log("Copied _redirects to dist folder");
+        } else {
+          console.warn("_redirects file does not exist in public folder");
+        }
+      },
+    },
+  ],
 });
